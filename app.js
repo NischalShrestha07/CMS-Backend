6d0223cb20c7acea44071834184211e114e49e9c
Nischal <nischalshrestha800@gmail.com>
2024-05-02 21:35:41 +0545

    mongoDB Details

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { connectDatabase } = require("./database/database")
 const Blog = require("./model/blogModel")
 
-const app=require("express")()
+const app = require("express")()
 
 // above line comes automatically when "connectDatabase()" is called.
 
@@ -11,36 +11,53 @@ const app=require("express")()
 connectDatabase()
 
 // GET API  -->  /
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-     status :200,
-     message:"Success"
+        status: 200,
+        message: "Success"
 
     })
 })
 
 // CREATE BLOG API
-app.post("/createBlog",async(req,res)=>{
-    // Insert to database logics goess here.
- 
+// app.post("/createBlog", async (req, res) => {
+//     // console.log(req.body);
+//     // Insert to database logics goess here.
 
-// Error Alert:when we call below function it should automatically add details at top
-await Blog.create({
-    title:req.body.title,
-    subTitle:req.body.subTitleitle,
-    description:req.body.description
+
+//     // Error Alert:when we call below "Blog" it should automatically add details at top
+//     await Blog.create({
+//         title: req.body.title,
+//         subTitle: req.body.subTitle,
+//         description: req.body.description
+//     })
+
+
+
+//     // res.json({
+//     //     status: 201,
+//     //     message: "Blog Created Successfully."
+//     // })
+
+// })
+
+app.post("/createBlog", (req, res) => {
+    console.log(req.body);
+
+    //     Blog.create({
+    //         title: req.body.title,
+    //         subTitle: req.body.subTitle,
+    //         description: req.body.description
+    //     })
+
+
+    //     res.json({
+    //         status: 201,
+    //         message: "Blog Created SuccessFully"
+    //     })
 })
 
-
-
-    res.json({
-        status:201,
-        message:"Blog Created Successfully."
-    })
-})
-
-
-app.listen(2000,()=>{
+app.listen(2000, () => {
     console.log("NodeJS has Started in port 2000");
 
 })
--- model/blogModel.js
+++ model/blogModel.js
@@ -1,21 +1,25 @@
 // This is called the Schema. We have to built it on our own but SQL gives automatically.
 
-const mongoose=require("mongoose")
-const blogSchema=new mongoose.Schema({
-    title:{  
-        type:String
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    title: {
+        type: String
     },
-    subTitle:{
-        type:String
+    subTitle: {
+        type: String
 
     },
-    description:{
-        type:String
-    }
-},{  
-    timestamps:true
+    description: {
+        type: String
+    },
+
+
+}, {
+    timestamps: true
 })
-const Blog=mongoose.model("Blog",blogSchema)
-module.exports=Blog
+const Blog = mongoose.model("Blog", blogSchema)
+module.exports = Blog
 // instead of two line above we can also write
 // model.exports=mongoose.model("Blog",blogSchema)
