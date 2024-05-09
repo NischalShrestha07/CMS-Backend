84f7d5d79ba6031f7a41c224a1c1663ab2b9d971
Nischal <nischalshrestha800@gmail.com>
2024-05-10 03:50:22 +0545

    Managing DataBase Connection

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const app = require("express")()
 const { connectDatabase } = require("./database/database")
+const Blog = require("./model/blogModel")
+const express = require("express")
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 // Database Connection Function:
@@ -14,6 +20,24 @@ app.get("/", (req, res) => {
 
 
 // create Blog API
+app.post("/createBlog", async (req, res) => {
+    console.log(req.body);
+    // Insert to database logic goes here
+    // await Blog.create({
+    //     title: req.body.title,
+    //     subTitle: req.body.subTitle,
+    //     description: req.body.description
+
+    // })
+
+
+
+
+    // res.json({
+    //     status: 200,
+    //     message: "Blog Created Successfully."
+    // })
+})
 
 app.listen(2000, () => {
     console.log("NodeJS has Started in port 2000");
