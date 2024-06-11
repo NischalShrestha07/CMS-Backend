daccc8493a710d40a80083233244f76b29fd62e2
Nischal <nischalshrestha800@gmail.com>
2024-06-11 10:24:53 +0545

    initial setups

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+//manage
 // Database Connection Function:
 connectDatabase()
 app.get("/", (req, res) => {
